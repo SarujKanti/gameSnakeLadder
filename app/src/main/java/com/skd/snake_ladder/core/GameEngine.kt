8b7e34de5509315db45ab2a0b03632fd7e8ea1ca
saruj <14skd12@gmail.com>
2026-02-16 16:43:34 +0530

    changes gameEngine position

--- app/src/main/java/com/skd/snake_ladder/core/GameEngine.kt
+++ app/src/main/java/com/skd/snake_ladder/core/GameEngine.kt
@@ -14,7 +14,6 @@ class GameEngine {
         }
 
         newPosition = SnakeLadderConfig.snakes[newPosition]
-
             ?: SnakeLadderConfig.ladders[newPosition]
                     ?: newPosition
 
