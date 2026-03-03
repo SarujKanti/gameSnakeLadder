2ed18df12e2cfa1baf4dad50dd318b7bcc4c9627
saruj <14skd12@gmail.com>
2026-03-03 11:41:05 +0530

    removed extra space

--- app/src/main/java/com/skd/snake_ladder/ui/view/GameScreen.kt
+++ app/src/main/java/com/skd/snake_ladder/ui/view/GameScreen.kt
@@ -89,7 +89,6 @@ fun GameScreen(viewModel: GameViewModel) {
             )
         )
         Spacer(modifier = Modifier.height(20.dp))
-
         DiceSection(
             diceValue = state.diceValue,
             isRolling = state.isRolling,
