31fa3424a000129be9e79e3095131f5ad733f4ec
saruj <14skd12@gmail.com>
2026-03-05 20:52:36 +0530

    removed extra space

--- app/src/main/java/com/skd/snake_ladder/ui/view/GameScreen.kt
+++ app/src/main/java/com/skd/snake_ladder/ui/view/GameScreen.kt
@@ -80,7 +80,6 @@ fun GameScreen(viewModel: GameViewModel) {
                 }
             }
         }
-
         Spacer(modifier = Modifier.height(20.dp))
         BoardCanvas(
             playerPositions = listOf(
